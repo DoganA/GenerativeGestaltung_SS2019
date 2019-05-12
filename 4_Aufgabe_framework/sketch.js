cf33650bd817961ca61154bf095b26ba94181281
Dogan <d.alk@hotmail.de>
2019-05-12 12:11:27 +0200

    Initial Aufgabe4

--- 4_Aufgabe_framework/sketch.js
+++ 4_Aufgabe_framework/sketch.js
@@ -5,6 +5,7 @@
 
 var fireworks = [];
 var gravity;
+var moving = true;
 
 function setup() {
   createCanvas(innerWidth, innerHeight);
@@ -13,22 +14,47 @@ function setup() {
   stroke(255);
   strokeWeight(4);
   background(0);
+   
 }
 
 function draw() {
   colorMode(RGB);
   background(0, 0, 0, 25);
   
+   //Create a Firework or "Rocket"
   if (random(1) < 0.03) {
     fireworks.push(new Firework());
   }
   
+   //Execute the firework
   for (var i = fireworks.length - 1; i >= 0; i--) {
     fireworks[i].update();
     fireworks[i].show();
     
+   //Delete exploaded 
     if (fireworks[i].done()) {
       fireworks.splice(i, 1);
     }
   }
+   
+   
+}
+
+function touchStarted() {
+	if(moving)
+	{
+		moving = false; 
+		strokeWeight(1);
+		fill(255);
+		rect(width/2-30, -height+20, 10, 30);
+		rect(width/2-50, -height+20, 10, 30);
+		noLoop();
+	}
+	else
+	{ 
+		moving = true;
+		loop();
+	} 
+	// prevent default
+  	return false;
 }
